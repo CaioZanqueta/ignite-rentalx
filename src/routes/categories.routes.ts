ed69c0672a7a9559ccb5c6bbd1b777913956afb3
Caio Zanqueta <caiozan2001@hotmail.com>
2022-04-12 01:06:01 -0300

    Adding Multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
-const cagoriesRoutes = Router();
+const categoriesRoutes = Router();
 
-cagoriesRoutes.post('/', (req, res) => {
+const upload = multer({ dest: './tmp' });
+
+categoriesRoutes.post('/', (req, res) => {
   return createCategoryController.handle(req, res);
 });
 
-cagoriesRoutes.get('/', (req, res) => {
+categoriesRoutes.get('/', (req, res) => {
   return listCategoriesController.handle(req, res);
 });
 
-export { cagoriesRoutes };
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  return importCategoryController.handle(req, res);
+});
+
+export { categoriesRoutes };
